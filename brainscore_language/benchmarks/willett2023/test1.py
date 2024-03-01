0aae0e3e69c1bff3fd93ae016545cdd6fbb286e5
hadarl <hadarl@gmail.com>
2024-03-01 09:29:12 -0500

    Added error bar scatter plot for score per layer. Added heatmap plots for neuroid scores. Added score histograms. Added electrode mapping from dataset readme file.

--- brainscore_language/benchmarks/willett2023/test1.py
+++ brainscore_language/benchmarks/willett2023/test1.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from brainscore_language.model_helpers.huggingface import HuggingfaceSubject
 from brainscore_language import ArtificialSubject
 
@@ -35,6 +37,7 @@ for layer in tqdm([f'transformer.h.{block}.ln_1' for block in range(36)], desc='
     layer_score = layer_score.expand_dims('layer')
     layer_score['layer'] = [layer]
     layer_scores.append(layer_score)
+layer_scores_backup = layer_scores
 layer_scores = merge_data_arrays(layer_scores)
 print(layer_scores)
 
@@ -47,7 +50,7 @@ print('Execution time:', elapsed_time, 'seconds')
 #########
 
 layer_names = []
-for layer in tqdm([f'transformer.h.{block}.ln_1' for block in range(36)], desc='layers'):
+for layer in tqdm([f'transformer.h.{block}.ln_1' for block in range(6)], desc='layers'):
     layer_names.append(layer)
 
 print(layer_names)
@@ -56,19 +59,55 @@ layer_scores_ordered = []
 for layer in layer_names:
     layer_scores_ordered.append(layer_scores.sel(layer=layer).data)
 
+layer_scores_std_ordered = []
+for layer in layer_names:
+    layer_scores_std_ordered.append(np.std(layer_scores.sel(layer=layer).raw.mean('neuroid')).data)
+
 print(layer_scores_ordered)
+print(layer_scores_std_ordered)
 
 import numpy as np
 from matplotlib import pyplot
 
-fig, ax = pyplot.subplots()
+fig, ax = pyplot.subplots(1,1,figsize=(5,5))
 x = np.arange(len(layer_scores_ordered))
-ax.scatter(x, layer_scores_ordered)
+#ax.scatter(x, layer_scores_ordered)
+ax.errorbar(x, layer_scores_ordered, yerr=layer_scores_std_ordered, fmt='o')
 ax.set_xticks(x)
-ax.set_xticklabels(layer_names, rotation=90)
+fig.subplots_adjust(bottom=0.3, left=0.2)
+ax.set_xticklabels(layer_names, rotation=45, ha='right')
 ax.set_ylabel('score')
 
-fig.savefig('Willett2023_gpt2-large_10Feb.png')
+fig.savefig('Willett2023_distilgpt.png')
+
+
+layer_scores_per_neuroid = []
+layer_scores_per_neuroid_reordered = []
+for layer in layer_names:
+    data = layer_scores.sel(layer=layer).raw.mean('split').data
+    data = data.reshape(256,1)
+    data_reordered = data[electrode_mapping]
+    print(np.shape(data))
+    layer_scores_per_neuroid.append(data)
+    layer_scores_per_neuroid_reordered.append(data_reordered)
+
+plt.imshow(layer_scores_per_neuroid_reordered[5].reshape(16,16))
+plt.colorbar()
+
+
+
+layer_scores_per_neuroid = []
+layer_scores_per_neuroid_reordered = []
+for layer in range(6):
+    data = layer_scores_backup[layer].raw.mean('split').data
+    data = data.reshape(256,1)
+    data_reordered = data[electrode_mapping]
+    layer_scores_per_neuroid.append(data)
+    layer_scores_per_neuroid_reordered.append(data_reordered)
+
+plt.imshow(layer_scores_per_neuroid_reordered[1].reshape(16,16))
+plt.colorbar()
+
 
 
 def plot_activations_per_word(assembly):
@@ -120,3 +159,175 @@ def plot_histogram_neuroid(assembly, index):
 
     # Show the plot
     plt.show()
+
+
+####
+
+ceilings = []
+for i in range(11):
+    ceilings.append(benchmark.ceiling[i].raw)
+ceilings = sum(ceilings, [])
+
+binwidth = (max(ceilings) - min(ceilings))/50
+for i in range(11):
+    plt.hist(benchmark.ceiling[i].raw, bins=np.arange(min(ceilings), max(ceilings) + binwidth, binwidth))
+    # plt.pause(0.5)
+n = [num for num in range(2, 24) if num % 2 == 0]
+plt.legend(n)
+
+plt.hist(ceilings,50)
+
+
+########
+
+# fig, axs = plt.subplots(2, 2)
+# plt.imshow(layer_scores_per_neuroid[0][0:64].reshape(8,8))
+
+def plot_scores_per_array(layer_scores_per_neuroid, electrode_mapping):
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    from matplotlib import colors
+
+    Nr = 2
+    Nc = 2
+
+    fig, axs = plt.subplots(Nr, Nc)
+    fig.suptitle('Scores per array')
+
+
+    images = []
+    for i in range(Nr):
+        for j in range(Nc):
+            # Generate data with a range that varies from one plot to the next.
+            array_ind = i*2 + j
+            #data = electrode_mapping
+            data = layer_scores_per_neuroid[array_ind][(array_ind*64):(array_ind+1)*64].reshape(8,8)
+            images.append(axs[i, j].imshow(data))
+            axs[i, j].label_outer()
+
+    # Find the min and max of all colors for use in setting the color scale.
+    vmin = min(image.get_array().min() for image in images)
+    vmax = max(image.get_array().max() for image in images)
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+    for im in images:
+        im.set_norm(norm)
+
+    fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1)
+
+
+    # Make images respond to changes in the norm of other images (e.g. via the
+    # "edit axis, curves and images parameters" GUI on Qt), but be careful not to
+    # recurse infinitely!
+    def update(changed_image):
+        for im in images:
+            if (changed_image.get_cmap() != im.get_cmap()
+                    or changed_image.get_clim() != im.get_clim()):
+                im.set_cmap(changed_image.get_cmap())
+                im.set_clim(changed_image.get_clim())
+
+
+    for im in images:
+        im.callbacks.connect('changed', update)
+
+    plt.show()
+
+
+
+def plot_scores_per_layer(layer_scores_per_neuroid_reordered, electrode_mapping):
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    from matplotlib import colors
+
+    Nr = 2
+    Nc = 3
+
+    fig, axs = plt.subplots(Nr, Nc)
+    fig.suptitle('Scores per layer')
+
+
+    images = []
+    for i in range(Nr):
+        for j in range(Nc):
+            # Generate data with a range that varies from one plot to the next.
+            layer_ind = i*Nc + j
+            #data = electrode_mapping
+            data = layer_scores_per_neuroid_reordered[layer_ind].reshape(16,16)
+            images.append(axs[i, j].imshow(data))
+            axs[i, j].label_outer()
+
+
+    # Find the min and max of all colors for use in setting the color scale.
+    vmin = min(image.get_array().min() for image in images)
+    vmax = max(image.get_array().max() for image in images)
+    norm = colors.Normalize(vmin=vmin, vmax=vmax)
+    for im in images:
+        im.set_norm(norm)
+
+    fig.colorbar(images[0], ax=axs, orientation='horizontal', fraction=.1)
+
+
+    # Make images respond to changes in the norm of other images (e.g. via the
+    # "edit axis, curves and images parameters" GUI on Qt), but be careful not to
+    # recurse infinitely!
+    def update(changed_image):
+        for im in images:
+            if (changed_image.get_cmap() != im.get_cmap()
+                    or changed_image.get_clim() != im.get_clim()):
+                im.set_cmap(changed_image.get_cmap())
+                im.set_clim(changed_image.get_clim())
+
+
+    for im in images:
+        im.callbacks.connect('changed', update)
+
+    plt.show()
+
+
+
+
+
+
+#####
+
+electrode_mapping = \
+               [192, 193, 208, 216, 160, 165, 178, 185,      62,  51,  43,  35,  94,  87,  79,  78,
+                194, 195, 209, 217, 162, 167, 180, 184,      60,  53,  41,  33,  95,  86,  77,  76,
+         		196, 197, 211, 218, 164, 170, 177, 189,      63,  54,  47,  44,  93,  84,  75,  74,
+				198, 199, 210, 219, 166, 174, 173, 187,      58,  55,  48,  40,  92,  85,  73,  72,
+				200, 201, 213, 220, 168, 176, 183, 186,      59,  45,  46,  38,  91,  82,  71,  70,
+				202, 203, 212, 221, 172, 175, 182, 191,      61,  49,  42,  36,  90,  83,  69,  68,
+				204, 205, 214, 223, 161, 169, 181, 188,      56,  52,  39,  34,  89,  81,  67,  66,
+				206, 207, 215, 222, 163, 171, 179, 190,      57,  50,  37,  32,  88,  80,  65,  64,
+                129, 144, 150, 158, 224, 232, 239, 255,     125, 126, 112, 103,  31,  28,  11,   8,
+				128, 142, 152, 145, 226, 233, 242, 241,     123, 124, 110, 102,  29,  26,   9,   5,
+				130, 135, 148, 149, 225, 234, 244, 243,     121, 122, 109, 101,  27,  19,  18,   4,
+				131, 138, 141, 151, 227, 235, 246, 245,     119, 120, 108, 100,  25,  15,  12,   6,
+				134, 140, 143, 153, 228, 236, 248, 247,     117, 118, 107,  99,  23,  13,  10,   3,
+				132, 146, 147, 155, 229, 237, 250, 249,     115, 116, 106,  97,  21,  20,   7,   2,
+				133, 137, 154, 157, 230, 238, 252, 251,     113, 114, 105,  98,  17,  24,  14,   0,
+				136, 139, 156, 159, 231, 240, 254, 253,     127, 111, 104,  96,  30,  22,  16,   1]
+
+
+db = {}
+db['model'] = model
+db['benchmark'] = benchmark
+db['layer_scores'] = layer_scores
+db['layer_score'] = layer_score
+
+dbfile = open('distilgpt2_Willett2023.pkl', 'ab')
+
+import pickle
+# source, destination
+pickle.dump(db, dbfile)
+dbfile.close()
+
+#
+# dbfile = open('examplePickle', 'rb')
+#     db = pickle.load(dbfile)
+#     for keys in db:
+#         print(keys, '=>', db[keys])
+#     dbfile.close()
